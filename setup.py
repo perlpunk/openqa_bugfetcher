7b811c1ccf8213b6a919b930236d8f7df77ce368
Dominik Heidler <dheidler@suse.de>
2017-09-22 15:43:49 +0200

    Handle error when fetching bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='openqa_bugfetcher',
-    version='0.0.4',
+    version='0.1.0',
     license='GPLv3',
     description='Tool to update the openqa bug status cache',
     author='Dominik Heidler',
