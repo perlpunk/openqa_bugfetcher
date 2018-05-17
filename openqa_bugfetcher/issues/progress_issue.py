e90de7d4a7b069e31d15a674b2aeab308a86fa80
Dominik Heidler <dheidler@suse.de>
2018-05-17 14:09:28 +0200

    Fix open status for reopened progress tickets

--- openqa_bugfetcher/issues/progress_issue.py
+++ openqa_bugfetcher/issues/progress_issue.py
@@ -16,8 +16,8 @@ class ProgressIssue(BaseIssue):
             self.priority = j['priority']['name']
             self.assigned = 'assigned_to' in j
             self.assignee = j['assigned_to']['name'] if self.assigned else None
-            self.open = 'closed_on' not in j
             self.status = j['status']['name']
+            self.open = self.status not in ('Rejected', 'Resolved', 'Closed')
         else:
             assert r.status_code != 401, "Wrong auth for Progress"
             self.existing = False
