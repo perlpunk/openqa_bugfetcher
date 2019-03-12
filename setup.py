84613bcd9a1f3322afa976979332fcaccd97b19b
Dominik Heidler <dheidler@suse.de>
2019-03-12 15:54:00 +0100

    Add basic webscraping support for bugzilla

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='openqa_bugfetcher',
-    version='0.3.2',
+    version='0.3.3',
     license='GPLv3',
     description='Tool to update the openqa bug status cache',
     author='Dominik Heidler',
