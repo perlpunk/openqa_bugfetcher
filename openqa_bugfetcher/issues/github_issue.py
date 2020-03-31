a06333a0ebc2babfe9b1ec9c0341feede2a924ee
root <root@openqa-service.suse.de>
2020-03-31 10:12:51 +0000

    Handle invalid github bugids

--- openqa_bugfetcher/issues/github_issue.py
+++ openqa_bugfetcher/issues/github_issue.py
@@ -7,19 +7,22 @@ class GitHubIssue(BaseIssue):
     prefixes = {'gh'}
 
     def fetch(self, conf):
-        repo, issue_id = self.bugid.split('#')[1:]
-        url = "https://api.github.com/repos/%s/issues/%s" % (repo, issue_id)
-        auth = None
-        if 'client_id' in conf['github'] and 'client_secret' in conf['github']:
-            auth = (conf['github']['client_id'], conf['github']['client_secret'])
-        r = requests.get(url, auth=auth)
-        assert r.status_code != 403, "Github ratelimiting"
-        if r.ok:
-            j = r.json()
-            self.title = j['title']
-            self.assigned = bool(j['assignee'])
-            self.assignee = j['assignee']['login'] if self.assigned else None
-            self.status = j['state']
-            self.open = self.status != "closed"
-        else:
+        try:
+            repo, issue_id = self.bugid.split('#')[1:]
+            url = "https://api.github.com/repos/%s/issues/%s" % (repo, issue_id)
+            auth = None
+            if 'client_id' in conf['github'] and 'client_secret' in conf['github']:
+                auth = (conf['github']['client_id'], conf['github']['client_secret'])
+            r = requests.get(url, auth=auth)
+            assert r.status_code != 403, "Github ratelimiting"
+            if r.ok:
+                j = r.json()
+                self.title = j['title']
+                self.assigned = bool(j['assignee'])
+                self.assignee = j['assignee']['login'] if self.assigned else None
+                self.status = j['state']
+                self.open = self.status != "closed"
+            else:
+                self.existing = False
+        except ValueError:
             self.existing = False
